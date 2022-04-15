a6b3db759cc932c7f887635c9d13ebd103ea08c7
Wanjiru393 <carolciru393@gmail.com>
2022-04-15 12:39:44 +0300

    Added Repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { UsernameComponent } from './username/username.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UsernameComponent,
+    RepositoryComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
