f84f024c4c7945e3ecc25be8facbb6b0e54e23a9
Wanjiru393 <carolciru393@gmail.com>
2022-04-17 01:30:28 +0300

    Created Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { UsernameComponent } from './username/username.component';
-import { RepositoryComponent } from './repository/repository.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
-import { DatePipe } from './date.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     UsernameComponent,
-    RepositoryComponent,
     RepositoriesComponent,
-    DatePipe
   ],
   imports: [
     BrowserModule,
