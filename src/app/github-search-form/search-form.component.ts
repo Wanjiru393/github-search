c67ecfb3a7c0e5169e54a2b11a27f7c0b275868b
Wanjiru393 <carolciru393@gmail.com>
2022-04-17 23:33:56 +0300

    changed data-binding

--- src/app/github-search-form/search-form.component.ts
+++ src/app/github-search-form/search-form.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
 
+
+
   constructor() { }
+
+  public searchUser(){
+
+  }
 
   ngOnInit(): void {
   }
